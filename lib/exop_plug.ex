81e3b93f9ab6fe5218435a2e8f5d6af6e66b7dc5
Andrey Chernykh <andrei.chernykh@gmail.com>
2019-10-23 10:05:43 +0300

    tak

--- lib/exop_plug.ex
+++ lib/exop_plug.ex
@@ -4,6 +4,8 @@ defmodule ExopPlug do
 
   defmacro __using__(_opts) do
     quote do
+      import unquote(__MODULE__)
+
       Module.register_attribute(__MODULE__, :contract, accumulate: true)
 
       @module_name __MODULE__
@@ -17,21 +19,37 @@ defmodule ExopPlug do
       if is_nil(@contract) || Enum.count(@contract) == 0 do
         file = String.to_charlist(__ENV__.file())
         line = __ENV__.line()
-        stacktrace = [{__MODULE__, :process, 1, [file: file, line: line]}]
-        msg = "An operation without a parameter definition"
+        stacktrace = [{__MODULE__, :init, 1, [file: file, line: line]}]
+        msg = "A plug without an action definition"
 
         IO.warn(msg, stacktrace)
       end
+
+      Enum.each(@contract, fn action_contract ->
+        param_opts = Map.get(action_contract, :params, %{})
+
+        if Enum.count(param_opts) == 0 do
+          file = String.to_charlist(__ENV__.file())
+          line = __ENV__.line()
+          stacktrace = [{__MODULE__, :init, 1, [file: file, line: line]}]
+          msg = "An action was defined without params specification"
+
+          IO.warn(msg, stacktrace)
+        end
+      end)
 
       @spec contract :: list(map())
       def contract, do: @contract
     end
   end
 
-  @spec parameter(atom() | binary(), keyword()) :: any()
-  defmacro parameter(name, opts \\ []) when is_atom(name) or is_binary(name) do
-    quote generated: true, bind_quoted: [name: name, opts: opts] do
-      @contract %{name: name, opts: [inner: opts]}
+  @spec action(atom() | binary(), keyword()) :: any()
+  defmacro action(action_name, opts \\ []) when is_atom(action_name) or is_binary(action_name) do
+    quote generated: true, bind_quoted: [action_name: action_name, opts: opts] do
+      opts = Enum.into(opts, %{})
+      params = Map.get(opts, :params, %{})
+
+      @contract %{action_name: action_name, opts: Map.put(opts, :params, params)}
     end
   end
 end
--- test/exop_plug_test.exs
+++ test/exop_plug_test.exs
@@ -4,7 +4,7 @@ defmodule ExopPlugTest do
   defmodule MyPlug do
     use ExopPlug
 
-    action :index, bypass: true
+    action :index # should be omitted
     action :show, params: %{user_id: [type: :integer]}
     # action :edit, params: %{user_id: [type: :integer], fields: [type: :map]}, on_fail: &__MODULE__.on_fail_func/2
   end
